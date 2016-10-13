a6422d377c128d4017c8df7d534879092d400fb2
Brandon Beckwith <bbeckwi2@uncc.edu>
2016-10-13 01:32:16 +0000

    Done with section 4.7.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,15 +5,23 @@ class MoviesController < ApplicationController
   end
   
   def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
+  	id = params[:id] # retrieve movie ID from URI route
+  	@movie = Movie.find(id) # look up movie by unique ID
+  	# will render app/views/movies/show.html.haml by default
   end
   
   def new
-  @movie = Movie.new
-  # default: render 'new' template
-	end 
+  	@movie = Movie.new
+  	# default: render 'new' template
+  end 
+	
+	def create
+  	#@movie = Movie.create!(params[:movie]) #old way
+  	@movie = Movie.create!(movie_params)  # new way
+  	flash[:notice] = "#{@movie.title} was successfully created."
+  	redirect_to movies_path
+  end
+  
   
   # add below all other methods
 	private
